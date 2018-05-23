b5b05d00309ad62fbade5c82c63a15957ff433e8
hanspeter6 <h.bakker@ru.ac.za>
2018-05-23 20:12:24 +0200

    7 factors update

--- amps08_ExploreLevel2_mins.R
+++ amps08_ExploreLevel2_mins.R
@@ -12,13 +12,13 @@ set08_min <- readRDS("/Users/HansPeter/Dropbox/Statistics/UCTDataScience/Thesis/
 set08_min <- set08_min[,-c(1:2,8:12,14:21)]
 
 # ## Determine Number of Factors to Extract
-# ev <- eigen(cor(set08_min[,7:ncol(set08_min)]))
-# ap <- parallel(subject=nrow(set08_min[,7:ncol(set08_min)]),var=ncol(set08_min[,7:ncol(set08_min)]),
-#                rep=100,cent=.02)
-# nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
-# jpeg("nScree_08_min")
-# plotnScree(nS, main = "National") # optimal = 6
-# dev.off()
+ev <- eigen(cor(set08_min[,7:ncol(set08_min)]))
+ap <- parallel(subject=nrow(set08_min[,7:ncol(set08_min)]),var=ncol(set08_min[,7:ncol(set08_min)]),
+               rep=100,cent=.02)
+nS <- nScree(x=ev$values, aparallel=ap$eigen$qevpea)
+jpeg("nScree_08_min")
+plotnScree(nS, main = "National") # optimal = 6
+dev.off()
 # 
 # # will set them at six for both Jhb and CT for now
 # npc <- 6
@@ -34,7 +34,7 @@ set08_min <- set08_min[,-c(1:2,8:12,14:21)]
 
 # pa method of factor analysis with oblimin rotation allowed....to try and get better estimation
 set.seed(123)
-fact_08 <- fa(set08_min[7:ncol(set08_min)], nfactors = 6, fm = "pa") # default rotation oblimin, so does allow correlation between factors
+fact_08 <- fa(set08_min[7:ncol(set08_min)], nfactors = 7, fm = "ml") # default rotation oblimin, so does allow correlation between factors
 fact_08_loadings <- fact_08$loadings
 fact_08_scores <- fact_08$scores
 
@@ -47,3 +47,4 @@ saveRDS(fact_08_loadings, "fact_08_loadings.rds")
 # save scores:
 saveRDS(fact_08_scores, "fact_08_scores.rds")
 
+write.csv(round(loadings(fact_08, sort = TRUE), 2), file = "loadings_min_08.csv")
